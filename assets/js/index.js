5ee4bdd491711ff7fec90214bde181a411e7000c
Miriam Peralta <miiriampc@gmail.com>
2017-06-27 14:18:25 -0500

    add student list

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ $(_=>{
     $.get('http://laboratoria.cuadra.co:9339/api/v1/students/',(req,res)=>{
         console.log(req);
         req.forEach((e)=>{
-            const name = $(`<li>${e.mLastName}<input type="checkbox"></li>`);
+            const name = $(`<li>${e.name}<input type="checkbox"></li>`);
             $('#student-list').append(name);
         })
     });
